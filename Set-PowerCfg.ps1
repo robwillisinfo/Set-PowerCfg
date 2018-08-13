5c3a395dcf8ee600a0ac160abda00b14544d170b
robwillisinfo <b1t_r0t@outlook.com>
2018-08-13 03:45:11 -0500

    Update Set-PowerCfg.ps1

--- Set-PowerCfg.ps1
+++ Set-PowerCfg.ps1
@@ -10,7 +10,7 @@ This simple script can be used to easily set or check the current power scheme b
 applied, these are the same settings found in Control Panel > Power Options.
 
 This is particularly useful on laptops where CPU throttling may be an issue and there 
-is a need to easily switch between power profiles, for exmaple - plugged in vs running on 
+is a need to easily switch between power profiles, for example - plugged in vs running on 
 battery.
 
 Author: Rob Willis (admin@robwillis.info)
